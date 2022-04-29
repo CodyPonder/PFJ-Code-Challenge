c0be43a317877b22cca59ab34322f44079d4b641
Cody Ponder <cponder@remwes.com>
2022-04-29 10:33:55 -0500

    Created OkHTTP client & API service

--- app/src/main/java/com/pilotflyingj/codechallenge/network/OkHttpProvider.kt
+++ app/src/main/java/com/pilotflyingj/codechallenge/network/OkHttpProvider.kt
@@ -1,11 +1,12 @@
 package com.pilotflyingj.codechallenge.network
 
+import okhttp3.OkHttpClient
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class OkHttpProvider @Inject constructor(
 
-){
-    // TODO: create the core OkHTTP client for reuse through the app
+) {
+	val okHttpClient = OkHttpClient.Builder().build()
 }
--- app/src/main/java/com/pilotflyingj/codechallenge/network/RetrofitServiceProvider.kt
+++ app/src/main/java/com/pilotflyingj/codechallenge/network/RetrofitServiceProvider.kt
@@ -1,11 +1,18 @@
 package com.pilotflyingj.codechallenge.network
 
+import retrofit2.Retrofit
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RetrofitServiceProvider @Inject constructor(
-
+	private val okHttpProvider: OkHttpProvider
 ) {
-    // TODO: create the API service lazily and retain in memory
+	val locationServiceProvider by lazy {
+		Retrofit.Builder()
+			.baseUrl("https://drive.google.com")
+			.client(okHttpProvider.okHttpClient)
+			.build()
+			.create(LocationService::class.java)
+    }
 }
